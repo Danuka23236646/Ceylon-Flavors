651f7296c9e04a43815afc3bb980ae507d5f48f5
Ushara Chamuditha Kumara <usharakumara20030820@gmail.com>
2025-03-16 22:40:15 +0530

    new

--- React-app/src/App.jsx
+++ React-app/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Menu from './component/Menu'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hello</h1>
+    <div id="wrapper">
+      <Menu></Menu>
+    </div>
     </>
   )
 }
